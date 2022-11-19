05554365d77fd60994e6aabc88da353b8b5b2987
ahteshammuz <117792802+ahteshammuz@users.noreply.github.com>
2022-11-19 23:00:48 +0530

    Update Assignment 2.cpp

--- Assignment 2.cpp
+++ Assignment 2.cpp
@@ -1,3 +1,5 @@
+    // USING FOR LOOP
+
 #include <iostream>
 using namespace std;
 int main() {
@@ -5,8 +7,6 @@ int main() {
   cout<<" Guys enter the number to know whether it is prime number or not"<< endl;
   cin >> n;
   int i=2 , c=0;
-
-    // Using for loop
 
 for(i=2; i<=n; i++){
     if(n%i==0){
@@ -23,9 +23,17 @@ for(i=2; i<=n; i++){
     }
   
   return 0;
-  
+}
 
-    //Using while loop
+   //USING WHILE LOOP
+
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  cout<<" Guys enter the number to know whether it is prime number or not"<< endl;
+  cin >> n;
+  int i=2 , c=0;
    
   while(i<=n)
 {
